10bd7c4e31daf3677b701ad9a2e7a2fa5f094769
notyung@naver.com <notyung@naver.com>
2024-04-10 11:14:26 +0900

    HTTP 요청 파라미터 - @RequestParam

--- src/main/java/hello/springMVC12/basic/request/RequestParamController.java
+++ src/main/java/hello/springMVC12/basic/request/RequestParamController.java
@@ -5,21 +5,22 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
 import java.io.IOException;
 import java.util.Locale;
+import java.util.Map;
 
 @Slf4j
 @RestController
 public class RequestParamController {
 
+    /**
+     반환 타입이 없으면서 이렇게 응답에 값을 직접 집어넣으면, view 조회X
+     */
     @RequestMapping("/request-param-v1")
     public void requestParamV1(HttpServletRequest request, HttpServletResponse response)throws IOException {
-
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
         log.info("username={}, age={}", username, age);
@@ -27,4 +28,93 @@ public class RequestParamController {
         response.getWriter().write("ok");
     }
 
+    /**
+     * @RequestParam 사용
+     * - 파라미터 이름으로 바인딩
+     * @ResponseBody 추가
+     * - View 조회를 무시하고, HTTP message body에 직접 해당 내용 입력
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge) {
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam 사용
+     * HTTP 파라미터 이름이 변수 이름과 같으면 @RequestParam(name="xx") 생략 가능
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(@RequestParam String username,
+                                 @RequestParam int age){
+        log.info("username ={}, age={}", username,age);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam 사용
+     * String, int 등의 단순 타입이면 @RequestParam 도 생략 가능
+     * @RequestParam 애노테이션을 생략하면 스프링 MVC는 내부에서 required=false를 적용한다.
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age){
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam.required
+     * /request-param -> username이 없으므로 예외
+     *
+     * 주의!
+     * /request-param?username= -> 빈문자로 통과
+     *
+     * 주의!
+     * /request-param
+     * int age -> null을 int에 입력하는 것은 불가능(500 예외 발생),
+     * 따라서 Integer로 변경해야 함(또는 다음에 나오는 defaultValue 사용)
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(@RequestParam(required = true) String username,
+                                       @RequestParam(required = false) Integer age){
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam
+     * - defaultValue 사용
+     *
+     * 파라미터에 값이 없는 경우 defaultValue를 사용하면 기본 값을 적용할 수 있다.
+     * 이미 기본 값이 있기 때문에 required는 의미가 없다.
+     *
+     * 참고: defaultValue는 빈 문자의 경우에도 적용
+     * /request-param?username=
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(@RequestParam(required = true, defaultValue = "gest") String username,
+                                      @RequestParam(required = false, defaultValue = "-1") int age){
+        log.info("username={}, age{}", username, age);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam Map, MultiValueMap
+     * Map(key=value)
+     * MultiValueMap(key=[value1, value2, ...] ex) (key=userIds, value=[id1, id2])
+     * 파라미터의 값이 1개가 확실하다면 Map을 사용해도 되지만, 그렇지 않다면 MultiValueMap을 사용하자.
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paraMap){
+        log.info("username={}, age={}", paraMap.get("username"), paraMap.get("age"));
+        return "ok";
+    }
 }
